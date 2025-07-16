4ed2e5bf3608ada54823e27c5c9d913d07246295
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-17 09:04:49 +1200

    Fixed the smoke, it now stops instantiation when the player picks up the burnt meat.

--- scripts/stove.gd
+++ scripts/stove.gd
@@ -1,9 +1,8 @@
 extends StaticBody3D
 var cooking_something = null
 
-# Preloads the particles for when meat is cooking on a stove.
+# Preloads the particles for when meat is burning on a stove.
 var particle_smoke = preload("res://prefabs/particle_smoke.tscn")
-var particle_sizzle = preload("res://prefabs/particle_sizzle.tscn")
 
 func _physics_process(_delta: float) -> void:
 	if cooking_something:
@@ -14,11 +13,9 @@ func _on_stove_body_entered(body: Node3D) -> void:
 		cooking_something = body
 		body.cooking = true
 	elif body.is_in_group("meat"):
-		print("worked")
 		var spawned_particle = particle_smoke.instantiate()
 		body.add_child(spawned_particle)
 		spawned_particle.global_position = body.global_position
-
 
 func _on_stove_body_exited(body: Node3D) -> void:
 	if body.is_in_group("cookable"):
@@ -26,5 +23,4 @@ func _on_stove_body_exited(body: Node3D) -> void:
 		body.cooking = false
 		$Label3D.text = ""
 	elif body.is_in_group("meat"):
-		print(body.get_children())
-		body.find_child('particle_smoke').queue_free()
+		body.get_child(2).queue_free()
