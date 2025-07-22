18e769d45f4289f706e50232f36c8d88a97770f0
Ben <22218@burnside.school.nz>
2025-07-22 22:16:26 +1200

    Ask Mr O'Sullivan how to code it in if it is clear confusing you.

--- scripts/trigger_body.gd
+++ scripts/trigger_body.gd
@@ -1,20 +1,38 @@
 extends Area3D
 
-@export var lava_ambience_sound:AudioStreamPlayer3D
+@onready var timer = $Timer
+@onready var audio_player = $"../AudioStreamPlayer3D_lava"
+
+func _ready():
+	self.body_entered.connect(_on_body_entered)
+	timer.timeout.connect(_on_timer_timeout)
 
 
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	lava_ambience_sound.play()
-	
+func _on_body_entered():
+	#timer.start(2.0)
+	audio_player.play()
 
 
-func _on_body_exited(body: Node3D) -> void:
-	lava_ambience_sound.stop()
+func _on_timer_timeout():
+	# Plays the ambiences  when the timer completes
+	audio_player.play()
 
 
-# Player enters the collision shape
-# Play audio ramp up from the animation track
-# when it is done, play the looping audio from auidio 3D
-# Once the player then leaves the collision shape, play the ramp_up animation in reverse
-# Finally, stop playing the audio once the track has finished
-# Animaton Documentation types
+#@export var lava_ambience_sound:AudioStreamPlayer3D
+#
+#
+#func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	#lava_ambience_sound.play()
+	#
+#
+#
+#func _on_body_exited(body: Node3D) -> void:
+	#lava_ambience_sound.stop()
+#
+#
+## Player enters the collision shape
+## Play audio ramp up from the animation track
+## when it is done, play the looping audio from auidio 3D
+## Once the player then leaves the collision shape, play the ramp_up animation in reverse
+## Finally, stop playing the audio once the track has finished
+## Animaton Documentation types
