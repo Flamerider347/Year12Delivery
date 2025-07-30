c418c842b953f577fc6b25fdb611efbdd13c2c29
Flamerider347 <flamerider995@gmail.com>
2025-07-31 08:35:07 +1200

    egg splat comimt

--- scripts/egg.gd
+++ scripts/egg.gd
@@ -1,9 +1,29 @@
 extends RigidBody3D
+
+var colliding = false
+var max_velocity = 0.0
 var type = "egg"
 var meat_cooked = preload("res://prefabs/egg_cracked.tscn")
+
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("jump_p1"):
-		var spawned_meat_cooked = meat_cooked.instantiate()
-		spawned_meat_cooked.position = position
-		get_parent().add_child(spawned_meat_cooked)
-		queue_free()
+	# Track peak speed
+	if linear_velocity.length() > max_velocity:
+		max_velocity = linear_velocity.length()
+
+	# Check for impact and break
+	if colliding and max_velocity > 2.5:
+		break_egg()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	colliding = true
+	print(body)
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	colliding = false
+	
+	
+func break_egg():
+	var cracked_egg = meat_cooked.instantiate()
+	cracked_egg.global_transform = global_transform
+	get_parent().add_child(cracked_egg)
+	queue_free()
