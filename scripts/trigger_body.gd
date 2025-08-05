c644a6cc75c102798d3e356e00301c97cd256ffb
22218-Burnside <22218@burnside.school.nz>
2025-08-05 21:45:00 +1200

    Quick merge into main so I can add the audio bus sliders onto the options menu

--- scripts/trigger_body.gd
+++ scripts/trigger_body.gd
@@ -7,6 +7,7 @@ extends Area3D
 
 func _on_body_entered(body):
 	# Creates tween variable audio manipulation
+	print("entered")
 	var tween = get_tree().create_tween()
 	# Checks if the body enter is a player
 	if body.name == "player_single" or body.name == "player" or body.name == "player2":
@@ -14,15 +15,19 @@ func _on_body_entered(body):
 		if $"../../..".level == 1:
 			pass
 		elif $"../../..".level == 2:
+			$AudioStreamPlayer3D_lava.play()
 			tween.tween_property($AudioStreamPlayer3D_lava, "volume_db", 0, 2)
 		elif $"../../..".level == 3:
+			$AudioStreamPlayer3D_underwater.play()
 			tween.tween_property($AudioStreamPlayer3D_underwater, "volume_db", 0, 2)
-		# elif $"../../..".level == 4:
+		#elif $"../../..".level == 4:
+			#$AudioStreamPlayer3D_frozen.play()
 			#tween.tween_property($AudioStreamPlayer3D_frozen, "volume_db", 0, 2)
 	else:
 		pass
 
 func _on_body_exited(body):
+	print("exited")
 	var tween = get_tree().create_tween()
 	# Checks if the body enter is a player
 	if body.name == "player_single" or body.name == "player" or body.name == "player2":
@@ -31,12 +36,13 @@ func _on_body_exited(body):
 			pass
 		elif $"../../..".level == 2:
 			tween.tween_property($AudioStreamPlayer3D_lava, "volume_db", -40, 2)
+			await get_tree().create_timer(1.0).timeout
+			$AudioStreamPlayer3D_lava.stop()
 		elif $"../../..".level == 3:
 			tween.tween_property($AudioStreamPlayer3D_underwater, "volume_db", -40, 2)
-		# elif $"../../..".level == 4:
+			await get_tree().create_timer(1.0).timeout
+			$AudioStreamPlayer3D_underwater.stop()
+		#elif $"../../..".level == 4:
 			#tween.tween_property($AudioStreamPlayer3D_frozen, "volume_db", -40, 2)
-	else:
-		pass
-		
-# Add timer which stops the audio from playing one the ramp off has been done.
-# then audio3D.stop()
+			#await get_tree().create_timer(1.0).timeout
+			$AudioStreamPlayer3D_frozen.stop()
