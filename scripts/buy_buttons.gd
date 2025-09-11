b460d5806d0840e0961876b70dd904be653674b7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-09-12 10:25:50 +1200

    All buttons now tween when having mouse hovering!!

--- scripts/buy_buttons.gd
+++ scripts/buy_buttons.gd
@@ -101,3 +101,15 @@ func _on_pressed() -> void:
 
 func button_money(cost):
 	text = "$" + str(cost)
+
+
+
+# Tween to increase the size of buttons when the mouse hovers
+func _on_mouse_entered() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1.2,1.2), 0.2).set_ease(Tween.EASE_OUT)
+
+# Tween to dencrease the size of buttons when the mouse stops hove
+func _on_mouse_exited() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1,1), 0.2).set_ease(Tween.EASE_OUT)
